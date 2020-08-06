7d206b27f8d24768cb25ff9f5d5e7a5639b4e717
Daniel Jesua Téllez Alvarado <DanielJesuaTellez@hotmail.com>
2020-08-06 11:10:20 -0500

    adding the functionality to choose until which fibonacci number is required

--- Fibonacci-pares.py
+++ Fibonacci-pares.py
@@ -1,10 +1,9 @@
-fibo = []
+fibo = [1,1]
 fibopar = []
-fibo.append (1)   #Necesito eliminar estos dos append, son una mala prática... 
-fibo.append (1)
 suma=0
 i=1
 j=0
+a=int(input("hasta que numero de fibonacci quieres ???\n"))
 while True:
     i = i +1
     fibo.append(fibo[i-1]+fibo[i-2])
@@ -12,7 +11,7 @@ while True:
         fibopar.append(fibo[i]);
         suma=suma+fibopar[j]
         j+=1
-    if i == 10:
+    if i == a:
         break
     
 
@@ -21,5 +20,3 @@ print("\n La lista de los fibonacci es:\n",fibo)
 print("\n La lista de los fibonacci par es:\n",fibopar)
 
 print("\n La suma de todos los numeros de fibonacci par es :",suma)
-
-
